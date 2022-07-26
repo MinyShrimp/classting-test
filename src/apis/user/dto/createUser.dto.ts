5a46c3b25c4cf77bd2f746aa5b37009b37369987
MinyShrimp <ksk7584@gmail.com>
2022-07-26 18:36:22 +0900

    :memo: swagger 설명 추가

--- src/apis/user/dto/createUser.dto.ts
+++ src/apis/user/dto/createUser.dto.ts
@@ -1,15 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, Length, Matches } from 'class-validator';
 
 export class CreateUserDto {
+    @ApiProperty({
+        example: '홍길동',
+        description: '이름',
+        required: true,
+    })
     @Length(2, 10)
     name: string;
 
+    @ApiProperty({
+        example: '호부호형',
+        description: '닉네임',
+        required: true,
+    })
     @Length(2, 8)
     nickName: string;
 
+    @ApiProperty({
+        example: 'qwer1234@gmail.com',
+        description: '이메일',
+        required: true,
+    })
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        example: 'qwer1234!',
+        description:
+            '8글자 이상, 영문자, 숫자, 특수문자가 1개 이상 포함된 비밀번호',
+        required: true,
+    })
     @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z])(?=.*[$!%*#?&^_-]).{8,}$/, {
         message: '8글자이상의 영문자 + 숫자 + 특수문자로 구성되어야 합니다.',
     })
--- src/apis/user/user.controller.ts
+++ src/apis/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+    ApiTags,
+    ApiOperation,
+    ApiOkResponse,
+    ApiConflictResponse,
+} from '@nestjs/swagger';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
 
+@ApiTags('회원')
 @Controller('api')
 export class UserController {
     constructor(
@@ -12,6 +19,12 @@ export class UserController {
      * 회원 가입
      */
     @Post('/signup')
+    @ApiOperation({
+        summary: '회원가입 API',
+        description: '유저 회원가입',
+    })
+    @ApiOkResponse({ description: 'Json Web Token' })
+    @ApiConflictResponse({ description: '이메일이나 닉네임이 중복되었습니다.' })
     async create(
         @Body() body: CreateUserDto, //
     ): Promise<string> {
