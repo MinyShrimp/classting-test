479cc1e4cdefe63c2d6acade9e7205656dd8355e
MinyShrimp <ksk7584@gmail.com>
2022-07-26 14:50:12 +0900

    :sparkles: swagger setting 추가

---
+++ src/commons/swagger/setup.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+/**
+ * Swagger 세팅
+ *
+ * @param {INestApplication} app
+ */
+export function setupSwagger(app: INestApplication): void {
+    const options = new DocumentBuilder()
+        .setTitle('클래스팅 API Docs')
+        .setDescription('클래스팅 사전 테스트 API Description')
+        .setVersion('1.0.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
+}
