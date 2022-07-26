d84c260742019f0d4dce38b02edc1ea046ed4aa0
MinyShrimp <ksk7584@gmail.com>
2022-07-26 14:49:52 +0900

    :sparkles: config, typeorm 설정 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,43 @@
+///////////////////////////////////////////////////////////////////////////
+// NestJS //
 import { Module } from '@nestjs/common';
 
+// TypeORM //
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Config //
+import { ConfigModule } from '@nestjs/config';
+
+///////////////////////////////////////////////////////////////////////////
 @Module({
-    imports: [],
+    imports: [
+        ///////////////////////////////////////////////////////////////////////////
+        // Enviroment Config //
+        // 최상단에 위치
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+        }),
+
+        ///////////////////////////////////////////////////////////////////////////
+        // TypeORM //
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: `${process.env.MYSQL_HOST}`,
+            port: 3306,
+            username: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+            entities: [
+                __dirname + '/apis/**/*.entity.*', //
+            ],
+
+            charset: 'utf8mb4',
+            collaction: 'utf8mb4_general_ci',
+            synchronize: true,
+            // logging: true,
+        }),
+    ],
     controllers: [],
     providers: [],
 })
